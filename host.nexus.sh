bb82882852e2061f2ee52e749b6069d8bf9f5317
hoso11 <hovsep.07@mail.ru>
2022-09-01 19:49:30 +0400

    changed

--- host.nexus.sh
+++ host.nexus.sh
@@ -35,4 +35,4 @@ sudo docker --version
 # docker login to nexus
 touch ~/my_password.txt
 echo "admin123" > ~/my_password.txt
-cat ~/my_password.txt | docker login --username admin --password-stdin
+cat ~/my_password.txt | docker login http://54.167.12.24:8082 --username admin --password-stdin
