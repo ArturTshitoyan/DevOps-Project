d3bc173b4c801036f869aa33e4ba0a91d430ce6a
hoso11 <hovsep.07@mail.ru>
2022-09-03 17:10:22 +0400

    v3

--- Docker_Login.sh
+++ Docker_Login.sh
@@ -1,4 +1,23 @@
 # docker login to nexus
+
+
+echo '{
+  "insecure-registries": [
+     "http://3.238.16.132:8081",
+     "http://3.238.16.132:8082"
+  ]
+}' > ~/daemon.json
+sudo mv ~/daemon.json /etc/docker/
+
+
 touch ~/my_password.txt
 echo "admin123" > ~/my_password.txt
-cat ~/my_password.txt | sudo docker login http://54.167.12.24:8082 --username admin --password-stdin
+cat ~/my_password.txt | sudo docker login http://3.238.16.132:8082 --username admin --password-stdin
+
+# tag and push to nexus
+
+sudo docker tag feb5d9fea6a5 3.238.16.132:8082/hello-world:mytag
+
+sudo docker push 3.238.16.132:8082/hello-world:mytag
+
+sudo docker pull  3.238.16.132:8081/hello-world:mytag
